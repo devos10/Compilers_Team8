f51652513d0218fb40581fbbd51799558168c07e
devos10 <osbaldo081004@gmail.com>
2025-11-30 19:50:22 -0600

    Estructura inicial de carpetas

--- Parser&STD/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='unam.fi.compilers.g5.08',
-    version='1.0.0',
-    package_dir={'': 'Parser&STD'},  # Indica dónde buscar
-    packages=find_packages(where='Parser&STD'),
-)
